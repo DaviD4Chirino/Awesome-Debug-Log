6c20385e4f22f3ecec2f2dcd852e410d5a09fb41
David Space <genarodavid@gmail.com>
2024-04-22 15:55:14 -0400

    installed update_button

--- addons/awesome_debug_log/awesome_debug_log.gd
+++ addons/awesome_debug_log/awesome_debug_log.gd
@@ -6,8 +6,17 @@ extends EditorPlugin
 const DEBUG_AUTOLOAD_NAME = "Debug"
 const DEBUG_AUTOLOAD_PATH: String = "res://addons/awesome_debug_log/autoloads/debug.tscn"
 
+# Update button
+const UPDATE_BUTTON_SCENE: PackedScene = preload ("res://addons/awesome_debug_log/editor/update button/update_button.tscn")
+var update_button: Button
+
 func _enter_tree() -> void:
 	add_autoload_singleton(DEBUG_AUTOLOAD_NAME, DEBUG_AUTOLOAD_PATH)
-
+	update_button = UPDATE_BUTTON_SCENE.instantiate()
+	update_button.editor_plugin = self
+	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, update_button)
+	
 func _exit_tree() -> void:
 	remove_autoload_singleton(DEBUG_AUTOLOAD_NAME)
+	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, update_button)
+	update_button.queue_free()
